794a4602dae65c9a9e38479d6248ca3121ccee64
raghava227 <nagireddyraghava9@gmail.com>
2021-06-02 12:13:36 +0530

    modified

--- src/components/Raghava.js
+++ src/components/Raghava.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 
-const Raghava = (props) =>{
+const Raghava reddy = (props) =>{
     return (
         <div>
 
@@ -11,4 +11,4 @@ const Raghava = (props) =>{
     );
 };
 
-export default Raghava;
+export default Raghava reddy;
